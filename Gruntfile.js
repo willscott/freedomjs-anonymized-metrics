ab1624861c1f55ae7b9813abca5e2490ac21a7fd
Will Scott <willscott@gmail.com>
2015-04-15 14:43:35 -0400

    require() / pull in rappor via browserify.

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+/*jslint node:true */
+
+module.exports = function (grunt) {
+  'use strict';
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      metrics: {
+        files: {
+          'metric.js': ['src/metric.js']
+        }
+      },
+      options: {
+        browserifyOptions: {
+          debug: true
+        }
+      }
+    },
+    jshint: {
+      all: ['src/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['build', 'test']);
+};
