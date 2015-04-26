0fa8b8a80a61f04ae4894276f7528ff03fd366ea
Will Scott <willscott@gmail.com>
2015-04-25 19:11:17 -0700

    in-module crypto shim shortcut for efficiency

--- src/crypto.js
+++ src/crypto.js
@@ -1,15 +1,25 @@
+/*jslint node:true,indent:2*/
+/*globals freedom,crypto,Uint8Array*/
 // crypto.getRandomBytes provider backed by the async freedom['core.crypto'].
+'use strict';
 
 var buf, offset = 0;
-var crypto = freedom['core.crypto']();
+var fcrypto = freedom['core.crypto']();
 
 /**
  * refresh the internal crypto buffer to have {buffer} bytes of randomness.
  * callback is called when buffer is refreshed.
  */
 exports.refreshBuffer = function (size, callback) {
-  crypto.getRandomvalues(size).then(function (b) {
-    buf = new UInt8Array(b);
+  if (typeof crypto !== 'undefined') {
+    buf = new Uint8Array(size);
+    crypto.getRandomValues(buf);
+    callback(0);
+    return;
+  }
+
+  fcrypto.getRandomvalues(size).then(function (b) {
+    buf = new Uint8Array(b);
     offset = 0;
     callback(0);
   }, function (err) {
@@ -23,10 +33,15 @@ exports.refreshBuffer = function (size, callback) {
  */
 exports.getRandomValues = function (buffer) {
   var size = buffer.length,
-      view = new UInt8Array(buffer),
-      i;
-      for (i = 0; i < size; i += 1) {
-        view[i] = buf[offset + i];
-      }
-      offset += size;
+    view = new Uint8Array(buffer),
+    i;
+
+  if (offset + size > buf.length) {
+    throw new Error("Insufficient Randomness Allocated.");
+  }
+
+  for (i = 0; i < size; i += 1) {
+    view[i] = buf[offset + i];
+  }
+  offset += size;
 };
