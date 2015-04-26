858977294e9447ca18c2bfdd2a78dcd42273036f
Will Scott <willscott@gmail.com>
2015-04-25 19:04:49 -0700

    initial thoughts on a crypto module

---
+++ src/crypto.js
@@ -0,0 +1,32 @@
+// crypto.getRandomBytes provider backed by the async freedom['core.crypto'].
+
+var buf, offset = 0;
+var crypto = freedom['core.crypto']();
+
+/**
+ * refresh the internal crypto buffer to have {buffer} bytes of randomness.
+ * callback is called when buffer is refreshed.
+ */
+exports.refreshBuffer = function (size, callback) {
+  crypto.getRandomvalues(size).then(function (b) {
+    buf = new UInt8Array(b);
+    offset = 0;
+    callback(0);
+  }, function (err) {
+    callback(-1, err);
+  });
+};
+
+/**
+ * fill an array buffer with random bytes, using the same interface as
+ * crypto.getRandomValues
+ */
+exports.getRandomValues = function (buffer) {
+  var size = buffer.length,
+      view = new UInt8Array(buffer),
+      i;
+      for (i = 0; i < size; i += 1) {
+        view[i] = buf[offset + i];
+      }
+      offset += size;
+};
